f0d3f36b39c659c1a6a8b1c85b91e65d1f954e29
jwentao <380793594@qq.com>
2019-10-27 23:04:59 +0800

    test

--- 1.two-sum.js
+++ 1.two-sum.js
@@ -4,7 +4,7 @@
  * @return {number[]}
  */
 let twoSum = function(nums, target) {
-    for (let i = 0; i < nums.length -1; i++) { // 暴力解法
+    for (let i = 0; i < nums.length -1; i++) { // 暴力解法 
         for (let j = i + 1; j < nums.length; j++) {
             if (nums[i] + nums[j] === target) {
                 return [i, j];
