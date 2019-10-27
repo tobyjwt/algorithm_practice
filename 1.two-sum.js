83df7d59055f4f691db490e8349b6e704a5cf820
jwentao <380793594@qq.com>
2019-10-27 23:06:58 +0800

    add

--- 1.two-sum.js
+++ 1.two-sum.js
@@ -3,13 +3,24 @@
  * @param {number} target
  * @return {number[]}
  */
-let twoSum = function(nums, target) {
-    for (let i = 0; i < nums.length -1; i++) { // 暴力解法 
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j];
-            }
+// let twoSum = function(nums, target) {
+//     for (let i = 0; i < nums.length -1; i++) { // 暴力解法
+//         for (let j = i + 1; j < nums.length; j++) {
+//             if (nums[i] + nums[j] === target) {
+//                 return [i, j];
+//             }
+//         }
+//     }
+// };
+
+var twoSum = function(nums, target) {
+    var temp = [];
+    for(var i=0;i<nums.length;i++){
+        var dif = target - nums[i];
+        if(temp[dif] != undefined){
+            return [temp[dif],i];
         }
+        temp[nums[i]] = i;
     }
 };
 
