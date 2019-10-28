d2950db7c20dcca9e4d0f751be6b7b5221f9dcb5
jwentao <380793594@qq.com>
2019-10-28 22:12:33 +0800

    temp

--- 1.two-sum.js
+++ 1.two-sum.js
@@ -13,12 +13,12 @@
 //     }
 // };
 
-var twoSum = function(nums, target) {
-    var temp = [];
-    for(var i=0;i<nums.length;i++){
-        var dif = target - nums[i];
-        if(temp[dif] != undefined){
-            return [temp[dif],i];
+let twoSum = function (nums, target) {
+    let temp = [];
+    for (let i = 0; i < nums.length; i++) {
+        let diff = target - nums[i];
+        if (temp[diff] !== undefined) {
+            return [temp[diff], i];
         }
         temp[nums[i]] = i;
     }
