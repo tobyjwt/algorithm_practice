34969c02016f915a6d3076984d64bd64ebd726b1
jinwentao <380793594@qq.com>
2021-12-03 18:18:53 +0800

    update 123

--- 123.best-time-to-buy-and-sell-stock-iii.js
+++ 123.best-time-to-buy-and-sell-stock-iii.js
@@ -8,25 +8,43 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    const n = prices.length;
-    const dp = new Array(n).fill(0).map(() => new Array(3).fill(0).map(() => new Array(2).fill(0)));
-    // dp[2][1][0]  第2天，已完成一次交易，持有股票
-    dp[0] = [
-        [-prices[0], 0],
-        [-Infinity, -Infinity],
-        [-Infinity, -Infinity]
-    ];
-    for (let i = 1; i < n; i++) {
-        dp[i][0][0] = Math.max(dp[i - 1][0][0], -prices[i]);
-        // dp[i][0][1] 无须更新
-        dp[i][1][0] = Math.max(dp[i - 1][1][0], dp[i - 1][1][1] - prices[i]);
-        dp[i][1][1] = Math.max(dp[i - 1][1][1], dp[i - 1][0][0] + prices[i]);
-        // dp[i][2][0] 无须更新
+    // const n = prices.length;
+    // const dp = new Array(n).fill(0).map(() => new Array(3).fill(0).map(() => new Array(2).fill(0)));
+    // // dp[2][1][0]  第2天，已完成一次交易，持有股票
+    // dp[0] = [
+    //     [-prices[0], 0],
+    //     [-Infinity, -Infinity],
+    //     [-Infinity, -Infinity]
+    // ];
+    // for (let i = 1; i < n; i++) {
+    //     dp[i][0][0] = Math.max(dp[i - 1][0][0], -prices[i]);
+    //     // dp[i][0][1] 无须更新
+    //     dp[i][1][0] = Math.max(dp[i - 1][1][0], dp[i - 1][1][1] - prices[i]);
+    //     dp[i][1][1] = Math.max(dp[i - 1][1][1], dp[i - 1][0][0] + prices[i]);
+    //     // dp[i][2][0] 无须更新
 
-        dp[i][2][1] = Math.max(dp[i - 1][2][1], dp[i - 1][1][0] + prices[i]);
+    //     dp[i][2][1] = Math.max(dp[i - 1][2][1], dp[i - 1][1][0] + prices[i]);
+    // }
+    // return Math.max(...dp[n - 1].flat());
+
+    // 更快更省
+    const n = prices.length;
+    const dp = new Array(n).fill([[], []]);
+    dp[0][0][0] = 0;
+    dp[0][1][0] = -prices[0];
+    dp[0][0][1] = -Infinity;
+    dp[0][1][1] = -Infinity;
+    dp[0][0][2] = -Infinity;
+    dp[0][1][2] = -Infinity;
+    for (let i = 1; i < n; i++) {
+        dp[i][0][0] = dp[i - 1][0][0];
+        dp[i][1][0] = Math.max(dp[i - 1][1][0], -prices[i]);
+        dp[i][0][1] = Math.max(dp[i - 1][0][1], dp[i - 1][1][0] + prices[i]);
+        dp[i][1][1] = Math.max(dp[i - 1][1][1], dp[i - 1][0][1] - prices[i]);
+        dp[i][0][2] = Math.max(dp[i - 1][0][2], dp[i - 1][1][1] + prices[i]);
+
     }
-    console.log(dp);
-    return Math.max(...dp[n - 1].flat());
+    return Math.max(...dp[n - 1][0]);
 };
 
 console.log(maxProfit([3,3,5,0,0,3,1,4])); // 6
