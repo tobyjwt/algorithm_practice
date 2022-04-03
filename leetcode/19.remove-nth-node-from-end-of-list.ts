c9c10b08275f21529f0d521f56b7ebfd36806e4c
jinwentao <jin_wentao@qq.com>
2022-04-03 15:48:57 +0800

    add 19

---
+++ leetcode/19.remove-nth-node-from-end-of-list.ts
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+ function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
+    let length = 0;
+    let cur = head;
+    while(cur) {
+        length++;
+        cur = cur.next;
+    }
+    if (n === length) {
+        return head.next;
+    }
+
+    if (n > length || n < 1) {
+        return head;
+    }
+
+    cur = head;
+    const m = length - n;
+    let idx = 0;
+    while(cur) {
+        if (idx === m - 1) {
+            if (cur.next) {
+                cur.next = cur.next.next;
+            } else {
+                cur.next = null;
+            }
+            break;
+        } else {
+            cur = cur.next;
+        }
+        idx++;
+    }
+    return head;
+};
