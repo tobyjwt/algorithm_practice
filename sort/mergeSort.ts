c336c8acfed6d3c313cbd40239d80f8d767c6c1c
jinwentao <jin_wentao@qq.com>
2022-04-30 18:23:45 +0800

    add ex

--- sort/mergeSort.ts
+++ sort/mergeSort.ts
@@ -26,6 +26,22 @@ function mergeSort(nums: number[]): number[] {
     }
     function sort(start: number, end: number): void {
         if (start < end) {
+            /*
+                 这里找到的middle为 start 和 end 的 "中位数"
+                     即 start = 5; end = 10; 则 middle = 7;
+                 需注意的是 这里采取的是向下舍入即:
+                     5，6，7，8，9，10 共 六个元素，则 middle=7; 5，6，7为一组；8，9，10为一组
+                     6，7，8，9，10 共五个元素，则 middle=8; 6，7，8为一组；9，10为一组
+
+
+                 公式为: Math.floor((end-start)/2) + start
+
+                 转换位运算符公式为：Math.floor(A / Math.pow(2, B)) => Math.floor(A / (2 ** B)) => (A >> B)
+
+
+                 所以:Math.floor((end-start)/2) + start === ((end - start)>>1) + start
+             */
+            // 折半分为两个小数组 并递归
             const mid = ((end - start) >> 1) + start;
             sort(start, mid);
             sort(mid + 1, end);
