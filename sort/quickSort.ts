2e3363d842a25c10d7bfd1e64d3c0d223efb4c80
jinwentao <jin_wentao@qq.com>
2022-05-07 22:07:05 +0800

    update quickSort

--- sort/quickSort.ts
+++ sort/quickSort.ts
@@ -1,23 +1,43 @@
 function quicklySort(nums: number[]): number[] {
-    
-    return sort(nums);
 
-    function sort(arr: number[]): number[] {
-        const n = arr.length;
-        if (n <= 1) {
-            return arr;
+    // return sort(nums);
+    //
+    // function sort(arr: number[]): number[] {
+    //     const n = arr.length;
+    //     if (n <= 1) {
+    //         return arr;
+    //     }
+    //     const left = [];
+    //     const right = [];
+    //     const mid = arr[0];
+    //     for (let i = 1; i <= n - 1; i++) {
+    //         if (arr[i] < mid) {
+    //             left.push(arr[i]);
+    //         } else {
+    //             right.push(arr[i]);
+    //         }
+    //     }
+    //     return [...sort(left), mid, ...sort(right)];
+    // }
+    sort(0, nums.length - 1);
+    return nums;
+
+    function sort(left: number, right: number) {
+        if(right <= left) {
+            return;
         }
-        const left = [];
-        const right = [];
-        const mid = arr[0];
-        for (let i = 1; i <= n - 1; i++) {
-            if (arr[i] < mid) {
-                left.push(arr[i]);
-            } else {
-                right.push(arr[i]);
+        let p = left;
+        const flag = nums[right];
+        for (let i = left ; i < right; i++) {
+            if (nums[i] < flag) {
+                [nums[i], nums[p]] = [nums[p], nums[i]];
+                p++;
             }
         }
-        return [...sort(left), mid, ...sort(right)];
+        [nums[p], nums[right]] = [nums[right], nums[p]];
+        const mid = Math.trunc((left + right) / 2);
+        sort(left, p - 1);
+        sort(p + 1, right);
     }
 }
 
